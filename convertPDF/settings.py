e052a18edf0c7d646bd018c9b27ae99a7ff265aa
Матвей Павлович Чекашов <60941066+Ryize@users.noreply.github.com>
2021-06-16 18:35:46 +0300

    Fix bug
    Fix bug on settings

--- convertPDF/settings.py
+++ convertPDF/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c#cu66p74+()en!mq!&ont+6f=!n!$a^opihpe6&xy-0^#82r5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
